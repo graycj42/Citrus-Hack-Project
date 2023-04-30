16d860983876c0feaefc54bdac617c49fd7b9662
Tommy Grounds <PuttyXD@gmail.com>
2023-04-29 17:13:29 -0700

    Added functionality to class

--- headers/password.hpp
+++ headers/password.hpp
@@ -1,13 +1,21 @@
-#ifndef password_H
-#define password_H
+#ifndef PASSWORD_H
+#define PASSWORD_H
+#include <string>
+
+using std::string;
 
 class Password
 {
 private:
+string savedPassword = "";
 
 public:
-Password();
+Password::Password(string userPassword);
 
+string Password::getSavedPassword();
+void Password::setSavedPassword(string givenPassword);
+
+bool Password::attemptLogin(string enteredPassword);
 
 };
 
